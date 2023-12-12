c3dee7cf69c28848deec528d105ca8e3ba5e3576
Murakam1q <kadomurakami6@gmail.com>
2023-12-12 04:33:33 +0300

    final ( надеюсь)

--- node.js
+++ node.js
@@ -6,36 +6,7 @@ $('.slider').slick({
     autoplay: true,
     dots:true,
     autoplaySpeed: 3000,
-    responsive: [
-        {
-            breakpoint: 1400,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '1px',
-                slidesToShow: 2,
-                centerPadding: '1px',
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
+  
 });
 
 
@@ -43,4 +14,5 @@ $('.slider__tablet').slick({
   dots:true,
   autoplaySpeed:3000,
   autoplay:true,
+  slidesToShow: 1,
 });
